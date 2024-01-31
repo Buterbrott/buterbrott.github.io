f7c9d40ab347967fb7ff78d128ac8814550f00c8
Buterbrott <bvorona@gmail.com>
2024-01-31 23:24:33 +0300

    Create hexgrid.js

---
+++ hexgrid.js
@@ -0,0 +1,35 @@
+
+const a = document.getElementById("hexgrid");
+const svgDoc = a.contentDocument.getElementById("layout");
+var svg = SVG(svgDoc);
+var leds = [];
+
+svg.each(function(i, children) {
+  if ( this.data('mapped') === undefined ){
+  } else {
+      leds[this.data('mapped')] = this;
+  }
+},true);
+
+let listener = setInterval(() => {
+    parent.postMessage({ app: 'wokwi', command: 'listen', version: 1 }, 'https://wokwi.com');
+}, 200);
+
+window.addEventListener('message', ({ data }) => {
+    if (data.neopixels) {
+      const { neopixels } = data;
+      for (let i = 0; i < neopixels.length; i++) {
+        const value = neopixels[i];
+        const b = value & 0xff;
+        const r = (value >> 8) & 0xff;
+        const g = (value >> 16) & 0xff;
+        if (leds[i]) {
+          leds[i].css({ fill: `rgb(${r}, ${g}, ${b})` })
+        }
+      }
+      if (listener != null) {
+        clearInterval(listener);
+        listener = null;
+      }
+    }
+});
